2a19cd6dac358c1a59dd8473e970ee122de8ca02
Xusen Yin <yinxusen@gmail.com>
2018-06-19 13:38:46 -0700

    refine eval

--- python/generation/model/evaluation.py
+++ python/generation/model/evaluation.py
@@ -1,12 +1,10 @@
-"""Tensorflow utility functions for evaluation"""
-
 import logging
 import os
 
 from tqdm import trange
 import tensorflow as tf
 
-from model.utils import save_dict_to_json
+from generation.model.utils import save_dict_to_json, steps_per_epoch
 
 
 def evaluate_sess(sess, model_spec, num_steps, writer=None, params=None):
@@ -14,48 +12,49 @@ def evaluate_sess(sess, model_spec, num_steps, writer=None, params=None):
 
     Args:
         sess: (tf.Session) current session
-        model_spec: (dict) contains the graph operations or nodes needed for training
+        model_spec: (dict) contains the graph operations or nodes for training
         num_steps: (int) train for this number of batches
-        writer: (tf.summary.FileWriter) writer for summaries. Is None if we don't log anything
+        writer: (tf.summary.FileWriter) writer for summaries.
         params: (Params) hyperparameters
     """
     update_metrics = model_spec['update_metrics']
     eval_metrics = model_spec['metrics']
     global_step = tf.train.get_global_step()
 
-    # Load the evaluation dataset into the pipeline and initialize the metrics init op
+    # Load the evaluation dataset into the pipeline
+    #  and initialize the metrics init op
     sess.run(model_spec['iterator_init_op'])
     sess.run(model_spec['metrics_init_op'])
 
     # compute metrics over the dataset
-    for _ in range(num_steps):
+    for _ in trange(num_steps):
         sess.run(update_metrics)
 
     # Get the values of the metrics
     metrics_values = {k: v[0] for k, v in eval_metrics.items()}
     metrics_val = sess.run(metrics_values)
-    metrics_string = " ; ".join("{}: {:05.3f}".format(k, v) for k, v in metrics_val.items())
-    logging.info("- Eval metrics: " + metrics_string)
+    metrics_string = " ; ".join(
+        "{}: {:05.3f}".format(k, v) for k, v in metrics_val.items())
+    logging.info("Eval metrics: " + metrics_string)
 
     # Add summaries manually to writer at global_step_val
     if writer is not None:
         global_step_val = sess.run(global_step)
         for tag, val in metrics_val.items():
-            summ = tf.Summary(value=[tf.Summary.Value(tag=tag, simple_value=val)])
+            summ = tf.Summary(
+                value=[tf.Summary.Value(tag=tag, simple_value=val)])
             writer.add_summary(summ, global_step_val)
-
     return metrics_val
 
 
 def evaluate(model_spec, model_dir, params, restore_from):
-    """Evaluate the model
-
-    Args:
-        model_spec: (dict) contains the graph operations or nodes needed for evaluation
-        model_dir: (string) directory containing config, weights and log
-        params: (Params) contains hyperparameters of the model.
-                Must define: num_epochs, train_size, batch_size, eval_size, save_summary_steps
-        restore_from: (string) directory or file containing weights to restore the graph
+    """
+    Evaluate a model
+    :param model_spec:
+    :param model_dir:
+    :param params:
+    :param restore_from:
+    :return:
     """
     # Initialize tf.Saver
     saver = tf.train.Saver()
@@ -71,8 +70,9 @@ def evaluate(model_spec, model_dir, params, restore_from):
         saver.restore(sess, save_path)
 
         # Evaluate
-        num_steps = (params.eval_size + params.batch_size - 1) // params.batch_size
+        num_steps = steps_per_epoch(params.eval_size, params.batch_size)
         metrics = evaluate_sess(sess, model_spec, num_steps)
         metrics_name = '_'.join(restore_from.split('/'))
-        save_path = os.path.join(model_dir, "metrics_test_{}.json".format(metrics_name))
+        save_path = os.path.join(model_dir,
+                                 "metrics_test_{}.json".format(metrics_name))
         save_dict_to_json(metrics, save_path)
